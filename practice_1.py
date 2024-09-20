164ec2b169ed5c2ed0c9120152e7f52fab8544ca
Shilpa-140 <shilpashilu140@gmail.com>
2024-09-20 14:11:10 +0530

    practice_files

--- practice_1.py
+++ practice_1.py
@@ -1,6 +1,5 @@
-def test_even():
-    num=4
-    if num%2==0:
-        print(f"{num} is even")
-    else:
-        print(f"{num} is odd")
+num=4
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
